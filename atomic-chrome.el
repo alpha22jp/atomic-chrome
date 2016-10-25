33db331257a3171d288f6617863b195515220d7c
alpha22jp <alpha22jp@gmail.com>
2016-10-26 07:44:56 +0900

    Add autoload cookie to interactive functions

--- atomic-chrome.el
+++ atomic-chrome.el
@@ -129,23 +129,6 @@ where FRAME show raw data received."
       (kill-buffer buffer))
     (remhash ws-conn atomic-chrome-ws-conn-list)))
 
-(defadvice save-buffers-kill-emacs
-      (before atomic-chrome-server-stop-before-kill-emacs)
-      "Call `atomic-chrome-close-server' before closing Emacs to avoid users \
-being prompted to kill the websocket server process."
-      (atomic-chrome-stop-server))
-
-(defun atomic-chrome-stop-server nil
-  "Stop websocket server for atomic-chrome."
-  (interactive)
-  (and atomic-chrome-server-conn
-       (websocket-server-close atomic-chrome-server-conn))
-  (ad-disable-advice 'save-buffers-kill-emacs
-                     'before 'atomic-chrome-server-stop-before-kill-emacs)
-  ;; Disabling advice doesn't take effect until you (re-)activate
-  ;; all advice for the function.
-  (ad-activate 'save-buffers-kill-emacs))
-
 (defvar atomic-chrome-edit-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-x C-s") 'atomic-chrome-send-buffer-text)
@@ -160,6 +143,13 @@ being prompted to kill the websocket server process."
   :init-value nil
   :keymap atomic-chrome-edit-mode-map)
 
+(defadvice save-buffers-kill-emacs
+      (before atomic-chrome-server-stop-before-kill-emacs)
+      "Call `atomic-chrome-close-server' before closing Emacs to avoid users \
+being prompted to kill the websocket server process."
+      (atomic-chrome-stop-server))
+
+;;;###autoload
 (defun atomic-chrome-start-server ()
   "Start websocket server for atomic-chrome."
   (interactive)
@@ -172,6 +162,18 @@ being prompted to kill the websocket server process."
            :on-message #'atomic-chrome-on-message
            :on-open nil
            :on-close #'atomic-chrome-on-close))))
+
+;;;###autoload
+(defun atomic-chrome-stop-server nil
+  "Stop websocket server for atomic-chrome."
+  (interactive)
+  (and atomic-chrome-server-conn
+       (websocket-server-close atomic-chrome-server-conn))
+  (ad-disable-advice 'save-buffers-kill-emacs
+                     'before 'atomic-chrome-server-stop-before-kill-emacs)
+  ;; Disabling advice doesn't take effect until you (re-)activate
+  ;; all advice for the function.
+  (ad-activate 'save-buffers-kill-emacs))
 
 (provide 'atomic-chrome)
 
