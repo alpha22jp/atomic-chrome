9115c5dbe87bffa7781369fbc851297ca89130d7
alpha22jp <alpha22jp@gmail.com>
2016-10-30 22:29:07 +0900

    Add commentary section

--- atomic-chrome.el
+++ atomic-chrome.el
@@ -24,6 +24,18 @@
 
 ;;; Commentary:
 
+;; This is the Emacs version of [Atomic Chrome](https://atom.io/packages/atomic-chrome)
+;; which is an extension for Google Chrome browser that allows you to edit text
+;; areas of the browser in Emacs.
+;;
+;; It's similar to [Edit with Emacs](https://www.emacswiki.org/emacs/Edit_with_Emacs),
+;; but has some advantages as below with the help of websocket.
+;;
+;; * Live update
+;;   The input on Emacs is reflected to the browser instantly and continuously.
+;; * Bi-directional communication
+;;   You can use both the browser and Emacs, they are synced to the same.
+
 ;;; Code:
 
 (require 'json)
